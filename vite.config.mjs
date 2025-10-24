5341a6c3e2e677e376ac0ef8703ba10905d75a60
giorgosvl <115508288+giorgosvl@users.noreply.github.com>
2025-10-24 14:59:55 +0300

    Fix Vite base and BrowserRouter for GitHub Pages

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // use relative base so Github Pages with repo or user pages works
+  base: '/portfolio/'
 
 })
